a528bb739d66f0e836f220be35ad3526d5a64e37
Asia <arj516@gmail.com>
2018-10-05 13:41:11 -0400

    add button

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function validateLunch(){
+function validateLunch(food){
+    var food = document.getElementById("lunchWeAe").textContent;
+    console.log(food);
     
+if (food === 'gyro') {
+    alert("My stomach is full.");
+
+} else {
+    alert("Im still hungry!");
+}
+
 }
